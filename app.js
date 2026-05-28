527b2a664df6f0601070758e9e95ccc08503e11e
Jair Sanchez <jair.sanchez.mx@gmail.com>
2026-05-28 11:32:01 -0600

    Agregue nueva funcionalidad

--- app.js
+++ app.js
@@ -4,13 +4,15 @@ for (i = 0 ; i <= 2 ; i ++){
 }
 console.log(arrayNumeros);
 let arrayNumerosReg = arrayNumeros.sort();
-console.log(arrayNumeros);
+console.log(`Los números de menor a mayor son: ${arrayNumeros}`);
 arrayNumerosReg.reverse();
-console.log(arrayNumerosReg);
-if (arrayNumerosReg[0] == arrayNumerosReg[1] == arrayNumerosReg[2]){
+console.log(`Los números de mayor a menor son: ${arrayNumerosReg}`);
+if (arrayNumerosReg[0] == arrayNumerosReg[1] && arrayNumerosReg[1] == arrayNumerosReg[2]){
     console.log("Los 3 números que ingresaste son iguales");
 } else if (arrayNumerosReg[0] == arrayNumerosReg[1]){
     console.log(`El número ${arrayNumerosReg[0]} esta repetido`);
-} else if (arrayNumerosReg[0] == arrayNumerosReg[2]){
+} else if  (arrayNumerosReg[0] == arrayNumerosReg[2]){
     console.log(`EL nuúmero ${arrayNumerosReg[2]} esta repetido`);
+} else {
+    arrayNumerosReg;
 }
