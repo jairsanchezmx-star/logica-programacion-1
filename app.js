2c84c66ce55a38305257987f4de3c96ae7e43b83
Jair Sanchez <jair.sanchez.mx@gmail.com>
2026-05-28 11:17:36 -0600

    Realice el ejercicio solictado

---
+++ app.js
@@ -0,0 +1,16 @@
+let arrayNumeros = [];
+for (i = 0 ; i <= 2 ; i ++){
+    arrayNumeros.push(Number(prompt("Ingresa un número")));
+}
+console.log(arrayNumeros);
+let arrayNumerosReg = arrayNumeros.sort();
+console.log(arrayNumeros);
+arrayNumerosReg.reverse();
+console.log(arrayNumerosReg);
+if (arrayNumerosReg[0] == arrayNumerosReg[1] == arrayNumerosReg[2]){
+    console.log("Los 3 números que ingresaste son iguales");
+} else if (arrayNumerosReg[0] == arrayNumerosReg[1]){
+    console.log(`El número ${arrayNumerosReg[0]} esta repetido`);
+} else if (arrayNumerosReg[0] == arrayNumerosReg[2]){
+    console.log(`EL nuúmero ${arrayNumerosReg[2]} esta repetido`);
+}
